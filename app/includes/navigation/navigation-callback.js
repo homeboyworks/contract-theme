b12765f3a70178cc6fb977c745b24c8b761610f6
Alexander <homeboyworks@gmail.com>
2016-04-25 20:46:53 +0400

    add class "opened" to toggle-button when collapse menu shows
    navbarCollapse.on(…..)

--- app/includes/navigation/navigation-callback.js
+++ app/includes/navigation/navigation-callback.js
@@ -1,5 +1,19 @@
 define(["jquery"], function ($) {
     return function () {
-        console.log("Navigation JS Callback executed")
+        console.log("Navigation JS Callback executed");
+
+        var navbarToggle = $('#navbar-toggle-id');
+        var navbarCollapse = $('#navbar-collapse-id');
+
+        navbarCollapse.on('show.bs.collapse', function () {
+
+            navbarToggle.addClass('opened');
+
+        });
+        navbarCollapse.on('hidden.bs.collapse', function () {
+
+            navbarToggle.removeClass('opened');
+
+        });
     }
 });
